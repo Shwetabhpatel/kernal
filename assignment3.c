5dd9015dd0ff4aaa0688ed8d1e0573deeeaf98ca
Shwetabhpatel <shwetabh17@gmail.com>
2024-03-01 22:57:48 +0530

    adding assignment

---
+++ assignment3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    float a,b;
+    printf("\n Enter first number");
+    scanf("%f",&a);
+
+    printf("\nEnter second number");
+    scanf("%f",&b);
+
+    printf("\n Addition is : %f",a+b);
+    printf("\n Subtraction is : %f",a-b);
+    printf("\n Multiplication is : %f",a*b);
+    printf("\n Division is : %f",a/b);
+    
+}
