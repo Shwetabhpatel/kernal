cd78ecdc508e8bbebb7d6b0ee25615c8861bd084
Shwetabhpatel <shwetabh17@gmail.com>
2024-03-01 19:25:42 +0530

    uploading assignment2

---
+++ assignment2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("\n Enter first number you want to add");
+    scanf("%d",&a);
+
+    printf("\n Enter second number you want to add");
+    scanf("%d",&b);
+
+    printf("\n Addition of two  numbers is : %d",a+b);
+}
