c5bbc6aff791669595abf9feeed1a46821103194
Shwetabhpatel <shwetabh17@gmail.com>
2024-03-04 18:37:27 +0530

    uploading assignment

---
+++ assignment5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("\n Enter the number whose table you want to print : ");
+    scanf("%d",&num);
+
+    for(int i=1;i<=10;i++)
+    {
+        printf("\n %d X %d = %d",num,i,num*i);
+    }
+
+}
+
+
