498e4fbf8f3faabbd220e9228ce739005a7050bc
Shwetabhpatel <shwetabh17@gmail.com>
2024-04-03 08:44:17 +0530

    adding goodbye function

---
+++ msg1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int printGoodbyeMessage() //function to declare message
+{
+    printf("\n Goodbye, and have a great day!");
+}
+void main()
+{
+   printGoodbyeMessage();//function caling
+}
