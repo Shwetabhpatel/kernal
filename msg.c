f8c8a5e6ad0b007193a86131fca4b3b81b4490b0
Shwetabhpatel <shwetabh17@gmail.com>
2024-04-03 08:31:09 +0530

    adding msg function program

---
+++ msg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int printWelcomeMessage() //function to declare message
+{
+    printf("\n Welcome to the programming class!");
+}
+void main()
+{
+   printWelcomeMessage();//function caling
+}
