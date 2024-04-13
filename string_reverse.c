28b7a63cf1a9dc10da4bdb8d5c20671f9d69e325
Shwetabhpatel <shwetabh17@gmail.com>
2024-04-13 14:09:07 +0530

    adding string reverse program

---
+++ string_reverse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+    char str[20] = "shwetabh";  //string named shwetabh
+ 
+    printf("\n The original string is =%s", str); //printing original string
+ 
+    printf("\n The reversed string is =%s", strrev(str));//printing reverse string
+ 
+}
